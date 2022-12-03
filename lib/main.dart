d5626b3ce82792074c15819fe0d2ff66edfecc0d
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-03 18:38:23 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
+
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  var directory = await getApplicationDocumentsDirectory();
-   Hive.init(directory.path);
-  runApp(const MyApp());
+ 
+  runApp(const Core());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+class Core extends StatelessWidget {
+  const Core({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Lava();
+  }
+}
+
+class Lava extends StatelessWidget {
+  const Lava({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Hive DB"),),
-      body: Column(
-        children: [
-
-        ],
-      ),
+      theme: ThemeData.dark(),
+      home: Scaffold(),
     );
     
   }
